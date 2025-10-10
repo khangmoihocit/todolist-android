dad08871e24e58adab9a48424e61fe62a85c52f9
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-10-10 18:19:30 +0700

    feat: add Category and Product controllers with DTOs for handling categories and products

---
+++ src/main/java/com/khangmoihocit/shopapp/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.khangmoihocit.shopapp.repositories;
+
+import com.khangmoihocit.shopapp.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
