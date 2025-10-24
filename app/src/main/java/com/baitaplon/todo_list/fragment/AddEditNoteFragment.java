04bd3a9af6ae4a0af9d81ed0238d0e818a2d11a8
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-10-24 16:50:16 +0700

    feat: Add AddEditNoteFragment for creating and editing notes
    - Implemented `AddEditNoteFragment` to handle the user interface for adding and editing notes.
    - Added XML layout `fragment_addedit_note.xml` with fields for title and content, and buttons for back, pin, and more options.
    - Integrated the fragment into `MainActivity`, allowing it to be displayed when the "add note" FAB is clicked.
    - Added new icons for add note, add schedule, pin, and back navigation.
    - Refactored styles for FABs and other UI components in `activity_main.xml`.
    - Created a skeleton `NoteAdapter`.

--- app/src/main/java/com/baitaplon/todo_list/fragment/AddEditNoteFragment.java
+++ app/src/main/java/com/baitaplon/todo_list/fragment/AddEditNoteFragment.java
@@ -1,4 +1,40 @@
 package com.baitaplon.todo_list.fragment;
 
-public class AddEditNoteFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.baitaplon.todo_list.R;
+
+public class AddEditNoteFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_addedit_note, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageView btnBack = view.findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(v -> closeFragment());
+    }
+
+    private void closeFragment() {
+        FragmentManager fm = getParentFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            fm.popBackStack();
+        } else if (getActivity() != null) {
+            getActivity().finish();
+        }
+    }
 }
